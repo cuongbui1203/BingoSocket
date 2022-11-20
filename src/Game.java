26afb0cd9f8ca6ff04eecfb4e3a1867f05efcb8f
monsieurtdn <83737033+monsieurtdn@users.noreply.github.com>
2022-11-20 23:01:55 +0700

    Add files via upload
    Cập nhật lại code logic

--- src/Game.java
+++ src/Game.java
@@ -16,50 +16,64 @@ public class Game {
         if(CoinToss == 1) {
             player1.status = 1;
             player2.setStatus(0);
-            System.out.println("Player 1's turn");
+            //System.out.println("Player 1's turn");
         }
         if(CoinToss == 2) {
             player2.status = 1;
             player1.setStatus(0);
-            System.out.println("Player 2's turn");
+            //System.out.println("Player 2's turn");
         }
     }
     static void Play() {
     while (player1.CheckWinCon() == false && player2.CheckWinCon() ==false) {
-        System.out.println("Insert your move: ");
+       
         
        // System.out.println(player1.getStatus()+" "+player2.getStatus());
         if(player1.getStatus() == 1) {  
-            player1.nhap();
+            System.out.println("Player 1's turn");
+            System.out.println("Insert your move: ");
 
+            player1.nhap();
             player2.move(player1.getN());
+
+            player1.CheckWinCon();
+            player2.CheckWinCon();
 
             if(player1.maxpoint >= 5) {
                 System.out.println("Player 1 win");
             }   else {
+
                 player2.setStatus(1);
                 player1.setStatus(0);
                 System.out.println("Player 1 point:"+" "+player1.maxpoint);
                 System.out.println("Player 2 point:"+ " "+ player2.maxpoint);
-                System.out.println("Player 2's turn");
+                
             }
             
         }else if(player2.getStatus() == 1) {
+            System.out.println("Player 2's turn");
+            System.out.println("Insert your move: ");
+
             player2.nhap();
             player1.move(player2.getN());
 
+            player1.CheckWinCon();
+            player2.CheckWinCon();
+
             if(player2.maxpoint >= 5) {
                 System.out.println("Player 2 win");
-                break;
+                
             }   else {
                 player1.setStatus(1);
                 player2.setStatus(0);
                 System.out.println("Player 1 point:"+" "+player1.maxpoint);
                 System.out.println("Player 2 point:"+ " "+ player2.maxpoint);
-                System.out.println("Player 1's turn");
+                
             }
             
         }
     }
+    if(player1.CheckWinCon() == true) System.out.println("Player 1 win");
+    if(player2.CheckWinCon() == true) System.out.println("Player 2 win");
 }
 }
